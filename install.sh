8cee8ef2efd45edf99a4f9286d01e7f84f226f27
Maxim Bolduc <max-bolduc@hotmail.com>
2022-08-15 12:52:42 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
+sudo dkms add -m rtl88x2bu -v ${VER}
+sudo dkms build -m rtl88x2bu -v ${VER} # Takes ~3-minutes on a 3B+
+sudo dkms install -m rtl88x2bu -v ${VER}
