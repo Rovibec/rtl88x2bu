14c0b7964c6cf578ef7e6934773ea6f4f469b3ac
Maxim Bolduc <max-bolduc@hotmail.com>
2022-08-15 13:02:27 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sed -i 's/I386_PC = y/I386_PC = n/' Makefile
-sed -i 's/ARM_RPI = n/ARM_RPI = y/' Makefile
+sudo sed -i 's/I386_PC = y/I386_PC = n/' Makefile
+sudo sed -i 's/ARM_RPI = n/ARM_RPI = y/' Makefile
 
-VER=$(sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
+VER=$(sudo sed -n 's/\PACKAGE_VERSION="\(.*\)"/\1/p' dkms.conf)
 sudo rsync -rvhP ./ /usr/src/rtl88x2bu-${VER}
 sudo dkms add -m rtl88x2bu -v ${VER}
 sudo dkms build -m rtl88x2bu -v ${VER} # Takes ~3-minutes on a 3B+
